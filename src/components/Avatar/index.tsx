7a13049c08276ddc54c5ad4b0c00ddf88fc100d3
Erick Marques <erick.marquesc@hotmail.com>
2023-01-18 19:39:20 -0300

    feat: :sparkles: created avatar component

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,9 @@
+import { AvatarConteiner } from "./styles";
+
+export function Avatar() {
+  return (
+    <AvatarConteiner>
+      <img src="https://github.com/erickmarquesc.png" />
+    </AvatarConteiner>
+  )
+}
---
+++ src/components/Avatar/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const AvatarConteiner = styled.div`
+  border-radius: 99px;
+  border: solid 2px white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img{
+    width: 112px;
+    height: 112px;
+    border-radius: 99px;
+  }
+`;
