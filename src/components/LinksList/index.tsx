2c68409aff4343ef691b0d70af4d808992707f4e
Erick Marques <erick.marquesc@hotmail.com>
2023-01-23 15:03:45 -0300

    feat: :sparkles: created link list

---
+++ src/components/LinksList/index.tsx
@@ -0,0 +1,17 @@
+export const LinkList = [
+  {
+    id: 1,
+    name: 'Conheça o meu GitHub',
+    url: 'https://github.com/erickmarquesc'
+  },
+  {
+    id: 2,
+    name: 'Conheça o meu Linkdin',
+    url: 'https://www.linkedin.com/in/erick-marques-cabral-999b151b4/'
+  },
+  {
+    id: 3,
+    name: 'Conheça o meu Instagram',
+    url: 'https://www.instagram.com/uierickmarques/'
+  },
+]
