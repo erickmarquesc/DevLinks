d624e06a32639fd5fb4e39d06387808a20a24df1
Erick Marques <erick.marquesc@hotmail.com>
2023-01-23 15:04:13 -0300

    feat: :sparkles: created home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,34 @@
+import { ButtonContainer } from "../components/Buttons/styles";
+import { ButtonLink } from "../components/Buttons";
+import { LinkList } from "../components/LinksList";
+import ImagePath from '../assets/background.svg';
 import { Avatar } from "../components/Avatar";
 import { Container, Content } from "./styles";
-import ImagePath from '../assets/background.svg'
 
 export function Home() {
   return (
     <Container img={ImagePath}>
       <Content>
-        <Avatar/>
-        <h1>ErickMarques</h1>
-      </Content>
+        <Avatar />
 
+        <h1>ErickMarques</h1>
+
+        <ButtonContainer>
+          {LinkList.map((link) => {
+            return (
+              <ButtonLink key={link.id}
+                nameButton={link.name}
+                urlButton={link.url}
+              />
+            )
+          })}
+        </ButtonContainer>
+
+        <strong>
+          Feito com muito ❤️ por Erick Marques <br />
+          Desenvolvedor Front-end UI Designer
+        </strong>
+      </Content>
     </Container>
   );
 };
--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -8,17 +8,25 @@ export const Container = styled.div<IContainerProps>`
   height: 100vh;
   padding: 2rem 0 2rem;
   background-image: url(${props => props.img});
+  min-width: 254px;
 `;
 
 export const Content = styled.div`
-  width: 100%;
   gap: 24px;
   display: flex;
   margin: 0 auto;
   max-width: 1120px;
+  min-width: 254px;
   padding: 0 1.5rem;
   text-align: center;
   align-items: center;
   flex-direction: column;
   justify-content: center;
+
+  h1{
+    font: normal 1rem ;
+  }
+  strong{
+    font: bold 0.25rem ;
+  }
 `;
