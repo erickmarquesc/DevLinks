04585d76a6e7ec25ea24b89b14f94c3073a296c9
Erick Marques <erick.marquesc@hotmail.com>
2023-03-20 19:55:41 -0300

    feat: :art: format of the code
    adicionei mais links ao projeto

--- src/components/LinksList/index.tsx
+++ src/components/LinksList/index.tsx
@@ -14,4 +14,24 @@ export const LinkList = [
     name: 'Conheça o meu Instagram',
     url: 'https://www.instagram.com/uierickmarques/'
   },
+  {
+    id: 4,
+    name: 'Conheça o meu projeto CoffeeDelivery',
+    url: 'https://coffee-delivery-chi-olive.vercel.app/'
+  },
+  {
+    id: 5,
+    name: 'Conheça o meu projeto TaskList',
+    url: 'https://task-list-to-do-neon.vercel.app/'
+  },
+  {
+    id: 6,
+    name: 'Conheça o meu projeto IgniteFeed',
+    url: 'https://ignite-feed-two-delta.vercel.app/'
+  },
+  {
+    id: 7,
+    name: 'Conheça o meu projeto GitHubBlog',
+    url: 'https://git-hub-blog-cyan.vercel.app/'
+  },
 ]
