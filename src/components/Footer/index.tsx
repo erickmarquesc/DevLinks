cd2deb73132ed824a3386adb81e4e212ad887eee
Erick Marques <erick.marquesc@hotmail.com>
2023-08-29 21:09:33 -0300

    feat: :sparkles: create a new component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import { FooterContainer } from "./styles"
+
+export function Footer() {
+  return (
+    <FooterContainer>
+      <p>
+        Feito com muito ❤️ por Erick Marques <br />
+        Desenvolvedor Front-end UI Designer
+      </p>
+    </FooterContainer>
+  )
+}
