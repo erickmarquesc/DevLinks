5470fb9fab839157d4b46b48313a72a103f16dba
Erick Marques <erick.marquesc@hotmail.com>
2023-08-29 21:08:44 -0300

    feat: :sparkles: create a new component

---
+++ src/utils/linkList.ts
@@ -0,0 +1,37 @@
+export const LinkList = [
+  {
+    id: 1,
+    name: 'Conheça o meu GitHub',
+    url: 'https://github.com/erickmarquesc'
+  },
+  {
+    id: 2,
+    name: 'Conheça o meu Linkdin',
+    url: 'https://www.linkedin.com/in/erick-marques-cabral-999b151b4/'
+  },
+  {
+    id: 3,
+    name: 'Conheça o meu Instagram',
+    url: 'https://www.instagram.com/uierickmarques/'
+  },
+  {
+    id: 4,
+    name: 'Conheça o meu projeto CoffeeDelivery',
+    url: 'https://coffeedeliveryecom.vercel.app/'
+  },
+  {
+    id: 5,
+    name: 'Conheça o meu projeto TaskList',
+    url: 'https://taskecomlisttodo.vercel.app/'
+  },
+  {
+    id: 6,
+    name: 'Conheça o meu projeto IgniteFeed',
+    url: 'https://igniteecomfeed.vercel.app/'
+  },
+  {
+    id: 7,
+    name: 'Conheça o meu projeto GitHubBlog',
+    url: 'https://githubblogecom.vercel.app/'
+  },
+]
