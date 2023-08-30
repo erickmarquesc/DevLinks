b0e34ddda8a167dd2f414055c5d5d54dc89e177c
Erick Marques <erick.marquesc@hotmail.com>
2023-08-29 21:07:25 -0300

    perf: :art: add new tag

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,9 +1,10 @@
-import { AvatarConteiner } from "./styles";
+import { AvatarConteiner } from "./styles"
 
 export function Avatar() {
   return (
     <AvatarConteiner>
       <img src="https://github.com/erickmarquesc.png" />
+      <h1>Erick Marques</h1>
     </AvatarConteiner>
   )
 }
